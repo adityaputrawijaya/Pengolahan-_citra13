c8cb5a09108dad949a1799cadb617fa1cc065d3d
adityaputrawijaya <115687055+adityaputrawijaya@users.noreply.github.com>
2024-06-06 14:27:25 +0700

    Add files via upload

---
+++ app2.py
@@ -0,0 +1,71 @@
+
+from PIL import Image
+
+def hide_text(image_path, secret_text, output_path):
+    # Open gambar
+    image = Image.open(image_path).convert("RGBA")  # Convert to RGBA to handle alpha channel
+ 
+    # Convert teks rahasia ke binari
+    binary_secret_text = ''.join(format(ord(char), '08b') for char in secret_text)
+ 
+    # Check if the image can accommodate the secret text
+    image_capacity = image.width * image.height * 3
+    if len(binary_secret_text) > image_capacity:
+        raise ValueError("Image does not have sufficient capacity to hide the secret text.")
+ 
+    # Menyembunyikan text rahasia kedalam gambar 
+    pixels = image.load()
+    index = 0
+    for i in range(image.width):
+        for j in range(image.height):
+            r, g, b, a = pixels[i, j]
+ 
+            # Modify the least significant bit of each color channel
+            if index < len(binary_secret_text):
+                r = (r & 0xFE) | int(binary_secret_text[index])
+                index += 1
+            if index < len(binary_secret_text):
+                g = (g & 0xFE) | int(binary_secret_text[index])
+                index += 1
+            if index < len(binary_secret_text):
+                b = (b & 0xFE) | int(binary_secret_text[index])
+                index += 1
+ 
+            pixels[i, j] = (r, g, b, a)
+ 
+    # Menyimpan gambar yang telah dimodifikasi
+    image.save(output_path)
+
+def extract_text(image_path):
+    # Open gambar
+    image = Image.open(image_path).convert("RGBA")  # Convert to RGBA to handle alpha channel
+ 
+    # Menampilkan text rahasia dari gambar
+    pixels = image.load()
+    binary_secret_text = ""
+    for i in range(image.width):
+        for j in range(image.height):
+            r, g, b, a = pixels[i, j]
+ 
+            # Extract bit paling tidak signifikan dari setiap saluran warna
+            binary_secret_text += str(r & 1)
+            binary_secret_text += str(g & 1)
+            binary_secret_text += str(b & 1)
+ 
+    # Mengubah teks binar menjadi ASCII
+    secret_text = ""
+    for i in range(0, len(binary_secret_text), 8):
+        char = chr(int(binary_secret_text[i:i+8], 2))
+        secret_text += char
+ 
+    return secret_text
+
+# Menyembunyikan teks rahasia dalam gambar
+image_path = 'bananaref.png'  # Jalur file gambar asli
+secret_text = 'This is a secret message.'
+output_path = 'bananaref.png'  # Jalur file gambar yang dihasilkan
+hide_text(image_path, secret_text, output_path)
+ 
+# Ekstraksi teks rahasia dari gambar
+extracted_text = extract_text(output_path)
+print("Extracted text:", extracted_text)
